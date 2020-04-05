ca5fe80a59a53e3bd4ee9934aa1635406afba549
Katzuno <erikhenning98@hotmail.com>
2020-04-05 17:19:02 +0300

    Built endpoint to upload video

--- routes.py
+++ routes.py
@@ -29,33 +29,42 @@ def create_new_folder(local_dir):
 
 @app.route('/upload/<gui_type>', methods=['POST'])
 def api_root(gui_type):
-    post_body = request.get_json(force=True)
-    if "base64string" in post_body:
-        scene_id = post_body['scene_id']
-        user_id = post_body['user_id']
-        element_type = post_body['element_type']
+    post_body = None
+    video = None
 
-        UPLOAD_PATH = os.path.join('scenes', 'scene_' + str(scene_id) + '_user_' + str(user_id))
-        UPLOAD_PATH = os.path.join(UPLOAD_PATH, 'Assets', gui_type)
-
-        imgdata = base64.b64decode(post_body['base64string'])
-        filename = element_type + '.jpg'
-        with open(os.path.join(UPLOAD_PATH, filename), 'wb') as f:
-            f.write(imgdata)
-
-        return json.dumps({'success': True})
+    if 'Content-type' in request.headers:
+        if request.headers.get('Content-type') == 'application/json':
+            post_body = request.get_json(force=True)
+        else:
+            post_body = request.form
+            if "video" not in request.files:
+                return jsonify(status=400, message='Video not found in multipart/form-data request')
+            video = request.files.get('video')
     else:
-        return json.dumps({"Error": "Img_url is missing"})
+        return jsonify(status=403, message='Content-type header not found')
 
+    print(post_body['element_type'])
+    scene_id = post_body['scene_id']
+    user_id = post_body['user_id']
+    element_type = post_body['element_type']
 
+    UPLOAD_PATH = os.path.join('scenes', 'scene_' + str(scene_id) + '_user_' + str(user_id))
+    UPLOAD_PATH = os.path.join(UPLOAD_PATH, 'Assets')
 
-    """
-    app.logger.info(UPLOAD_PATH)
-    img = request.files['image']
-    img_name = secure_filename(img.filename)
-    create_new_folder(UPLOAD_PATH)
-    saved_path = os.path.join(UPLOAD_PATH, img_name)
-    app.logger.info("saving {}".format(saved_path))
-    img.save(saved_path)
-    return jsonify({'success': True})
-    """
+    if gui_type != 'cursor' and gui_type != 'video':
+        UPLOAD_PATH = os.path.join(UPLOAD_PATH, gui_type)
+
+    if not video:
+        if "base64string" in post_body:
+            img_data = base64.b64decode(post_body['base64string'])
+            filename = element_type + '.jpg'
+            with open(os.path.join(UPLOAD_PATH, filename), 'wb') as f:
+                f.write(img_data)
+        else:
+            return jsonify(Error="Image or video is missing")
+    else:
+        filename = element_type + '-2.mov'
+        with open(os.path.join(UPLOAD_PATH, filename), 'wb') as f:
+            f.write(video.read())
+
+    return jsonify(success=True, uploaded=gui_type, element_type=element_type)
