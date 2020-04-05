a8045270d9cb4ba4d2e9f86fdc2a979e412bfb21
Katzuno <erikhenning98@hotmail.com>
2020-04-05 12:00:53 +0300

    Built endpoint to upload gui elements

--- __init__.py
+++ __init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from jwtauth.database import init_db, shutdown_db_session
-from os import environ
+import os
+
+SCENES_FOLDER = 'scenes'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.config['SCENE_FOLDER'] = os.path.join(os.path.dirname(os.path.realpath(__file__)), SCENES_FOLDER)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.teardown_appcontext
@@ -13,6 +21,6 @@ def shutdown_session(exception=None):
 from . import routes
 
 if __name__ == '__main__':
-    app.config['SECRET_KEY'] = environ.get('JWT_SECRET_KEY')
+    app.config['SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
     init_db()
     app.run()
--- routes.py
+++ routes.py
@@ -1,14 +1,61 @@
+import os
+
 from flask_jwt import jwt_required
 from jwtauth.jwt import *
 from flask_jwt import JWT
+import json
+from flask import flash, request, redirect, url_for, send_from_directory, jsonify
+from werkzeug.utils import secure_filename
+from . import app, allowed_file
+import base64
 
-
-from . import app
 
 @app.route('/hello')
 def hello():
     return 'Hello world!'
 
-@app.route('/auth', methods = ['POST'])
+
+@app.route('/auth', methods=['POST'])
 def auth():
     return 'Hello world'
+
+
+def create_new_folder(local_dir):
+    newpath = local_dir
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+    return newpath
+
+
+@app.route('/upload/<gui_type>', methods=['POST'])
+def api_root(gui_type):
+    post_body = request.get_json(force=True)
+    if "base64string" in post_body:
+        scene_id = post_body['scene_id']
+        user_id = post_body['user_id']
+        element_type = post_body['element_type']
+
+        UPLOAD_PATH = os.path.join('scenes', 'scene_' + str(scene_id) + '_user_' + str(user_id))
+        UPLOAD_PATH = os.path.join(UPLOAD_PATH, 'Assets', gui_type)
+
+        imgdata = base64.b64decode(post_body['base64string'])
+        filename = element_type + '.jpg'
+        with open(os.path.join(UPLOAD_PATH, filename), 'wb') as f:
+            f.write(imgdata)
+
+        return json.dumps({'success': True})
+    else:
+        return json.dumps({"Error": "Img_url is missing"})
+
+
+
+    """
+    app.logger.info(UPLOAD_PATH)
+    img = request.files['image']
+    img_name = secure_filename(img.filename)
+    create_new_folder(UPLOAD_PATH)
+    saved_path = os.path.join(UPLOAD_PATH, img_name)
+    app.logger.info("saving {}".format(saved_path))
+    img.save(saved_path)
+    return jsonify({'success': True})
+    """
